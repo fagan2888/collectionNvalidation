d351462a88a432e82e80a6a75834ae7080a4c983
shashi1920 <shashikant.sharma.cse12@iitbhu.ac.in>
2015-06-17 00:08:53 +0530

    Added Models

--- collect/collect/settings.py
+++ collect/collect/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'collection',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -46,7 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'collection',
+
 )
 
 ROOT_URLCONF = 'collect.urls'
--- collect/collection/models.py
+++ collect/collection/models.py
@@ -1,63 +1,61 @@
 from django.db import models
-class profile(models.Model):
-    email=models.EmailField(null=False,blank=False,primary_key=True)
-    name=models.CharField(max_length=150)
-    level=models.IntegerField(max_length=1)
-    dept_name=models.CharField(max_length=150)
 
-class professor(models.Model):
-    prof_id=models.AutoField(primary_key=True)
-    prof_name=models.CharField(max_length=150)
-    email=models.EmailField(blank=False,null=False)
 
-class approved_course_list(models.Model):
-    course_code=models.CharField(max_length=10,primary_key=True)
-    course_name=models.CharField(max_length=150)
-    syllabus=models.CharField(max_length=2500)
-    level_0=models.ForeignKey(profile,null=True,blank=True)
-    level_1=models.ForeignKey(profile,null=True,blank=True)
-    level_2=models.ForeignKey(profile,null=True,blank=True)
+class Profile(models.Model):
+    email = models.EmailField(null=False, blank=False, primary_key=True)
+    name = models.CharField(max_length=150)
+    level = models.IntegerField(max_length=1)
+    dept_name = models.CharField(max_length=150)
 
-class proposed_course_list(models.Model):
-    course_code=models.CharField(max_length=10,primary_key=True)
-    course_name=models.CharField(max_length=150)
-    syllabus=models.CharField(max_length=2500)
-    level_0=models.ForeignKey(profile,null=True,blank=True)
-    level_1=models.ForeignKey(profile,null=True,blank=True)
-    level_2=models.ForeignKey(profile,null=True,blank=True)
-    level_0_sign=models.IntegerField(max_length=1)
-    level_1_sign=models.IntegerField(max_length=1)
-    level_2_sign=models.IntegerField(max_length=1)
 
-class approved_course_teaching(models.Model):
-    application_no=models.AutoField(primary_key=True)
-    course_code=models.ForeignKey(approved_course_list)
-    prof_id=models.ForeignKey(professor)
-    semester=models.IntegerField(max_length=2)
-    branch=models.CharField(max_length=3)
-    course=models.CharField(max_length=10)
+class Professor(models.Model):
+    prof_id = models.AutoField(primary_key=True)
+    prof_name = models.CharField(max_length=150)
+    email = models.EmailField(blank=False, null=False)
 
-class approved_course_teaching(models.Model):
-    application_no=models.AutoField(primary_key=True)
-    course_code=models.ForeignKey(approved_course_list)
-    prof_id=models.ForeignKey(professor)
-    semester=models.IntegerField(max_length=2)
-    branch=models.CharField(max_length=3)
-    course=models.CharField(max_length=10)
-    level_0=models.ForeignKey(profile,null=True,blank=True)
-    level_1=models.ForeignKey(profile,null=True,blank=True)
-    level_2=models.ForeignKey(profile,null=True,blank=True)
 
-class proposed_course_teaching(models.Model):
-    application_no=models.AutoField(primary_key=True)
-    course_code=models.ForeignKey(approved_course_list)
-    prof_id=models.ForeignKey(professor)
-    semester=models.IntegerField(max_length=2)
-    branch=models.CharField(max_length=3)
-    course=models.CharField(max_length=10)
-    level_0=models.ForeignKey(profile,null=True,blank=True)
-    level_1=models.ForeignKey(profile,null=True,blank=True)
-    level_2=models.ForeignKey(profile,null=True,blank=True)
-    level_0_sign=models.IntegerField(max_length=1)
-    level_1_sign=models.IntegerField(max_length=1)
-    level_2_sign=models.IntegerField(max_length=1)
+class ApprovedCourseList(models.Model):
+    course_code = models.CharField(max_length=10, primary_key=True)
+    course_name = models.CharField(max_length=150)
+    syllabus = models.CharField(max_length=2500)
+    level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_0_list')
+    level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_1_list')
+    level_2 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_2_list')
+
+
+class ProposedCourseList(models.Model):
+    course_code = models.CharField(max_length=10, primary_key=True)
+    course_name = models.CharField(max_length=150)
+    syllabus = models.CharField(max_length=2500)
+    level_0 = models.ForeignKey(Profile, null=True, blank=True,related_name='%(class)_level_0_list')
+    level_1 = models.ForeignKey(Profile, null=True, blank=True,related_name='%(class)_level_1_list')
+    level_2 = models.ForeignKey(Profile, null=True, blank=True,related_name='%(class)_level_2_list')
+    level_0_sign = models.IntegerField(max_length=1)
+    level_1_sign = models.IntegerField(max_length=1)
+    level_2_sign = models.IntegerField(max_length=1)
+
+class ApprovedCourseTeaching(models.Model):
+    application_no = models.AutoField(primary_key=True)
+    course_code = models.ForeignKey(ApprovedCourseList)
+    prof_id = models.ForeignKey(Professor)
+    semester = models.IntegerField(max_length=2)
+    branch = models.CharField(max_length=3)
+    course = models.CharField(max_length=10)
+    level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_0_teaching')
+    level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_1_teaching')
+    level_2 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_2_teaching')
+
+
+class ProposedCourseTeaching(models.Model):
+    application_no = models.AutoField(primary_key=True)
+    course_code = models.ForeignKey(ApprovedCourseList)
+    prof_id = models.ForeignKey(Professor)
+    semester = models.IntegerField(max_length=2)
+    branch = models.CharField(max_length=3)
+    course = models.CharField(max_length=10)
+    level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_0_teaching')
+    level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_0_teaching')
+    level_2 = models.ForeignKey(Profile, null=True, blank=True, related_name='%(class)_level_0_teaching')
+    level_0_sign = models.IntegerField(max_length=1)
+    level_1_sign = models.IntegerField(max_length=1)
+    level_2_sign = models.IntegerField(max_length=1)
